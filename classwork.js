e92902e124d2320790abcfe9f5503ed22784429b
TsMariam <tsaturovamariam@gmail.com>
2019-02-05 19:00:06 +0400

    File edited. Create third commit.

--- classwork.js
+++ classwork.js
@@ -31,3 +31,42 @@ if (x === y) {
         console.log(0);
     }
 }
+
+//3.	Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of the angles of a triangle equals 180 degrees).
+
+let x = 45;
+let y = 90;
+
+console.log(180 - (x + y));
+
+//4.	Given a positive integer. Bring the last digit of the number to the beginning. Print the new number. If the last digit of the inserted number is 0, number remains the same.
+
+let x = 367;
+let n, a;
+
+if (x % 10 == 0 || x % 10 == x) {
+    console.log(x);
+} else {
+    n = 0;
+    a = x;
+    while (x != 0) {
+        n++;
+        x = (x - x % 10) / 10;
+    }
+    console.log(a % 10 * Math.pow(10, n - 1) + (a - a % 10) / 10);
+}
+
+let c = 250;
+
+if (c % 10 == 0 || c % 10 == c) {
+    console.log(c);
+} else {
+    n = 0;
+    a = c;
+    while (y != 0) {
+        n++;
+        d = (d - d % 10) / 10;
+    }
+    console.log(a % 10 * Math.pow(10, n - 1) + (a - a % 10) / 10);
+}
+
