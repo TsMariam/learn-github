1b4a3197d5c42afdda2c22aefe839397acc0caae
TsMariam <tsaturovamariam@gmail.com>
2019-02-05 18:43:38 +0400

    Add classwork file. Create first commit.

---
+++ classwork.js
@@ -0,0 +1,11 @@
+//1.	Given a number. Print “odd” if the number is odd and “even” if it’s even.
+
+let x = 123;
+
+if (x % 2 == 1) {
+    console.log("odd");
+}
+else {
+    console.log("Even");
+}
+
